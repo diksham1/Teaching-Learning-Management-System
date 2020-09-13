d20fd72a6a636f22351ca1389b512c2ad424eea5
Diksha <dikshamodi28@gmail.com>
2020-09-13 21:23:56 +0530

    feat: create login ui

---
+++ client/tlms/src/routes.js
@@ -0,0 +1,14 @@
+const base = 'localhost:3000'
+
+const ROUTES = {
+    'student': {
+        'dashboard': '/dashboard'
+    },
+    'api' :{
+        'post' : {
+            'login': ''
+        }
+    }
+}
+
+export default ROUTES;
