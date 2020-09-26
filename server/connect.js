77c3d84d8bea6d85855f85579ed909c3b9659c29
Diksha <dikshamodi28@gmail.com>
2020-09-26 18:55:23 +0530

    chore: setup database and sample data

---
+++ server/connect.js
@@ -0,0 +1,42 @@
+const MongoClient = require('mongodb').MongoClient;
+const uri = "mongodb+srv://[username]:[password]@tlms.1so97.mongodb.net/tlms?retryWrites=true&w=majority";
+const client = new MongoClient(uri, { useNewUrlParser: true });
+
+const dbName = "tlms";
+
+ async function run() {
+    try {
+         await client.connect();
+         console.log("Connected correctly to server");
+         const db = client.db(dbName);
+
+         // Use the collection "people"
+         const col = db.collection("user");
+
+         // Construct a document                                                                                                                                                              
+         let personDocument = {
+             "name": { "first": "Alan", "last": "Turing" },
+             "birth": new Date(1912, 5, 23), // June 23, 1912                                                                                                                                 
+             "death": new Date(1954, 5, 7),  // June 7, 1954                                                                                                                                  
+             "contribs": [ "Turing machine", "Turing test", "Turingery" ],
+             "views": 1250000
+         }
+
+         // Insert a single document, wait for promise so we can read it back
+         const p = await col.insertOne(personDocument);
+         // Find one document
+         const myDoc = await col.findOne();
+         // Print to the console
+         console.log(myDoc);
+
+        } catch (err) {
+         console.log(err.stack);
+     }
+ 
+     finally {
+        await client.close();
+    }
+}
+
+run().catch(console.dir);
+
--- server/v1.js
+++ server/v1.js
@@ -1,8 +1,19 @@
 const express = require('express')
 const router = express.Router()
+const MongoClient = require('mongodb').MongoClient;
 
 router.get('/login', function (req, res) {
 	// login code here
+	/*
+const uri = "mongodb+srv://tlms:<password>@tlms.1so97.mongodb.net/<dbname>?retryWrites=true&w=majority";
+const client = new MongoClient(uri, { useNewUrlParser: true });
+client.connect(err => {
+  const collection = client.db("test").collection("devices");
+  // perform actions on the collection object
+  client.close();
+});
+
+*/
 	console.log(req)
 	res.json({'statusCode': 200});
 })
