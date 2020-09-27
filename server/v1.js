048daaafbae54e9b7e2e3f3f943a2fbdbc724f17
Diksha <dikshamodi28@gmail.com>
2020-09-27 22:07:25 +0530

    chore: remove node_modules from repo

--- server/v1.js
+++ server/v1.js
@@ -1,10 +1,13 @@
 const express = require('express')
 const router = express.Router()
 const dbconn = require('./dbconn')
+const createInviteCode = require('./createInviteCode')
+const createLiveClassLink = require('./createLiveClassLink')
 
-router.get('/login', function (req, res) {	
-	res.json({'statusCode': 200});
-})
+const errorOccuredResponse = {
+	"status": 500,
+	"result": false
+}
 
 /**
  * Creates a new user entry in the database
@@ -24,10 +27,47 @@ router.post('/users', async function(req, res) {
 		});	
 	} catch(e) {
 		console.log(e);
-		return res.json({
-			"statuscode": "500",
-			"result": false
-		})
+		return res.json(errorOccuredResponse)
+	}
+})
+
+
+/**
+ * @todo create live class links with webRTC
+ * @todo Use logger to record error
+ * @todo Improve invite code creation algorithm
+ * Create a new course and return class link and invite code
+ */
+router.post('/courses', async function(req, res) {
+	try {
+		const db = await dbconn();
+		const col = db.collection('Course')
+		let inviteCode = null;
+		let liveClassLink = null;
+		
+		do {
+			inviteCode = await createInviteCode();
+			console.log(inviteCode);
+		} while (col.findOne({"invite_code": inviteCode}) != null);
+			
+		liveClassLink = await createLiveClassLink();
+		
+		req.body["invite_code"] = inviteCode;
+		req.body["live_class_link"] = liveClassLink;
+		
+		const insertResponse = await col.insertOne(req.body);
+	
+		const responseJSON = {
+			"statuscode": 201,
+			"course_id": insertResponse["ops"][0]["_id"],
+			"invite_code": inviteCode,			
+			"live_class_link": liveClassLink,
+			"result": true
+		}
+		return res.json(responseJSON);
+	} catch (e) {
+		console.log(e)
+		return res.json(errorOccuredResponse)
 	}
 })
 
