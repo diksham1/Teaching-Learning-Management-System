4ddbca0d9e9cb83f2ebca0a1c0f694636aa87ebc
Kunal Ojha <kunalojha1999@gmail.com>
2020-10-06 23:20:05 +0530

    Refactor : Post Creation UI

---
+++ client/tlms/src/Components/Mypost.js
@@ -0,0 +1,70 @@
+import React from 'react'
+
+export default function Mypost(props){
+
+    function mypost_focus() {
+      if (document.getElementById("mypost").innerHTML === "Write a Post") {
+        document.getElementById("mypost").innerHTML = "";
+      }
+    }
+    function mypost_blur() {
+      if (document.getElementById("mypost").innerHTML === "") {
+        document.getElementById("mypost").innerHTML = "Write a Post";
+      }
+    }
+    function mypost_title_focus() {
+      if (
+        document.getElementById("myposttitle").innerHTML === "Post Title Here"
+      ) {
+        document.getElementById("myposttitle").innerHTML = "";
+      }
+    }
+    function mypost_title_blur() {
+      if (document.getElementById("myposttitle").innerHTML === "") {
+        document.getElementById("myposttitle").innerHTML = "Post Title Here";
+      }
+    } 
+
+    return (
+      <div class="w-full shadow-xl hover:shadow-2xl">
+        <div class="bg-gray-700 text-gray-200 p-4 cursor-pointer text-2xl">
+          You
+        </div>
+        <div>
+          <div
+            class="w-full bg-gray-100 text-black p-4 text-lg text-opacity-50 border-b-2 border-gray-400 border-solid"
+            id="myposttitle"
+            contentEditable="true"
+            onFocus={mypost_title_focus}
+            onBlur={mypost_title_blur}
+          >
+            Post Title Here
+          </div>
+          <div
+            class="w-full bg-gray-100 text-black p-4 text-lg text-opacity-50"
+            id="mypost"
+            contentEditable="true"
+            onFocus={mypost_focus}
+            onBlur={mypost_blur}
+          >
+            Write a Post
+          </div>
+          <div class="w-full bg-gray-100 text-white p-4 text-lg flex flex-row justify-end">
+            <button class="rounded-lg text-center w-4/12 p-2 mx-2 bg-green-600 hover:opacity-75 font-semibold"
+                style = {{
+                    display : (props.isTeacher)?"":"none"
+                }}
+            >
+              Mark As Assignment
+            </button>
+            <button class="rounded-lg text-center w-2/12 p-2 mx-2 bg-green-600 hover:opacity-75 font-semibold">
+              Attach File
+            </button>
+            <button class="rounded-lg text-center w-2/12 p-2 mx-2 bg-red-600 hover:opacity-75 font-semibold">
+              Post
+            </button>
+          </div>
+        </div>
+      </div>
+    );
+}
