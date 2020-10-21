8e7824ce22231110f8fb4a82ebbe8f6dd83f4a39
Kunal Ojha <kunalojha1999@gmail.com>
2020-10-21 11:20:21 +0530

    😶😶😶😶😶😶

--- client/tlms/src/index.js
+++ client/tlms/src/index.js
@@ -4,7 +4,7 @@ import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
 
-ReactDOM.hydrate(
+ReactDOM.render(
   <App />,
   document.getElementById('root')
 );
