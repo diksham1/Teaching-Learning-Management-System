491e004ccf7279f9d07e16f5a635f2f4762fe480
Kunal Ojha <kunalojha1999@gmail.com>
2020-10-21 11:17:16 +0530

    chore : make authorization context available

--- client/tlms/src/App.js
+++ client/tlms/src/App.js
@@ -1,30 +1,39 @@
-import React from 'react';
+import React,{useState} from 'react';
 import './App.css';
 import Login from './Components/Login';
 import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
 import DashBoard from './Components/Dashboard';
 import Class from './Components/Class';
 import ClassTeacher from "./Components/ClassTeacher"
-import AuthContextProvider from "./Contexts/AuthContext.js"
+import AuthContextProvider from "./Contexts/AuthContext"
+
 
 function App() {
   return (
-    <AuthContextProvider>
-      <Router>
+    <Router>
         <Switch>
-          <Route path="/" exact component={Login} />
+          <Route path="/" exact>
+            <AuthContextProvider>
+              <Login />
+            </AuthContextProvider>
+          </Route>
+          <Route path="/dashboard" exact>
+            <AuthContextProvider>
+              <DashBoard />
+            </AuthContextProvider>
+          </Route>
+          <Route path="/class" exact>
+            <AuthContextProvider>
+              <Class />
+            </AuthContextProvider>
+          </Route>
+          <Route path="/class2" exact>
+            <AuthContextProvider>
+              <ClassTeacher />
+            </AuthContextProvider>
+          </Route>
         </Switch>
-        <Switch>
-          <Route path="/dashboard" exact component={DashBoard} />
-        </Switch>
-        <Switch>
-          <Route path="/class" exact component={Class} />
-        </Switch>
-        <Switch>
-          <Route path="/class2" exact component={ClassTeacher} />
-        </Switch>
-      </Router>
-    </AuthContextProvider>
+    </Router>
   );
 }
 
--- client/tlms/src/Contexts/AuthContext.js
+++ client/tlms/src/Contexts/AuthContext.js
@@ -1,16 +1,40 @@
-import React, { Component, createContext } from "react";
+import React, { Component, createContext} from "react";
 
 export const AuthContext = createContext();
 
 class AuthContextProvider extends Component {
   state = {
-    isLoggedIn: false,
-    email: "",
-    name: ""
+    isLoggedIn_state: false,
+    isEducator_state: false,
+    email_state: "",
+    name_state: ""
   };
+
+  toggleisLoggedIn_state = () => {
+    this.setState({ isLoggedIn_state : !this.state.isLoggedIn_state})
+  }
+
+  toggleisEducator_state = () => {
+    this.setState({ isEducator_state : !this.state.isEducator_state})
+  }
+
+  setemail_state = (p) => {
+    this.setState( {email_state : p} )
+  }
+
+  setname_state = (p) => {
+    this.setState( {name_state : p} )
+  }
+
   render() {
     return (
-      <AuthContext.Provider value={{...this.state}}>
+      <AuthContext.Provider value={
+        {...this.state, 
+          toggleisLoggedIn_state : this.toggleisLoggedIn_state,
+          setemail_state : this.setemail_state,
+          setname_state : this.setname_state,
+          toggleisEducator_state : this.toggleisEducator_state
+        }}>
         {this.props.children}
       </AuthContext.Provider>
     );
