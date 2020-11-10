395b760fe67b2c9e7d11291027fba039f65455e7
Kunal Ojha <kunalojha1999@gmail.com>
2020-11-11 01:52:27 +0530

    chore : file simplified for use

--- client/tlms/src/Components/FilePanel.js
+++ client/tlms/src/Components/FilePanel.js
@@ -2,20 +2,10 @@ import React from 'react'
 
 export default function FilePanel(props){
     return (
-      <div class="w-full flex flex-row">
-        <div class="w-6/12 text-left pl-2">{props.name}</div>
-        <div class="w-5/12 text-right">
+        <div class="w-full">
           <a class="underline pr-2" href={props.fileurl}>
             {props.filename}
           </a>
         </div>
-        <button class="flex flex-row justify-center itesm-end w-1/12">
-          <div class = "w-4/12">
-            <svg viewBox="0 0 40 40" preserveAspectRatio="xMidYMid meet">
-              <path d="M 0 0 L 40 40 M 0 40 L 40 0" stroke="black"></path>
-            </svg>
-          </div>
-        </button>
-      </div>
     );
 }
